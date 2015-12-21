548e9ab38dabb35747bb982ba6bb855582a11d14
Saurab Dulal <dulal.saurab@gmail.com>
2015-12-21 23:35:38 +0545

    Update formSubmission.php

--- formSubmission.php
+++ formSubmission.php
@@ -51,7 +51,7 @@
           <ul class="nav navbar-nav">
             <li class=""><a href="index.php">Home</a></li>
             <li class="active" ><a href="formSubmission.php">SubmitYourForm </a></li>
-            <li><a href="#contact">Contact</a></li>
+            <!--<li><a href="#contact">Contact</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown Test<span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -63,7 +63,7 @@
                 <li><a href="#">Separated link</a></li>
                 <li><a href="#">One more separated link</a></li>
               </ul>
-            </li>
+            </li> -->
           </ul>
         </div><!--/.nav-collapse -->
       </div>
